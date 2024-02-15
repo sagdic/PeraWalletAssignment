0eab66c3c142ff044602a07753597ae0aa5ffccc
U0T14893 <tayfun256sagdic@gmail.com>
2024-02-15 15:46:09 +0300

    Initial Commit

---
+++ PeraWalletAssignment/PeraWalletAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeraWalletAssignmentApp.swift
+//  PeraWalletAssignment
+//
+//  Created by Tayfun Sagdic on 15.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct PeraWalletAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
