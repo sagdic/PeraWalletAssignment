dcb9da2f037b5e97449e4cfd9d8b20142e6d822d
U0T14893 <tayfun256sagdic@gmail.com>
2024-02-22 15:38:21 +0300

    FavoritesView added.

--- PeraWalletAssignment/Repository/View/FavoritesView.swift
+++ PeraWalletAssignment/Repository/View/FavoritesView.swift
@@ -5,4 +5,25 @@
 //  Created by Tayfun Sagdic on 22.02.2024.
 //
 
-import Foundation
+import SwiftUI
+
+struct FavoritesView: View {
+    @ObservedObject var viewModel: RepositoryViewModel
+    
+    var body: some View {
+        NavigationView {
+            List(viewModel.favoriteRepositories) { repo in
+                NavigationLink(destination: RepositoryDetailView(repository: repo)) {
+                    VStack(alignment: .leading) {
+                        Text(repo.name)
+                            .font(.headline)
+                        Text(repo.description ?? "Empty Description")
+                            .font(.subheadline)
+                    }
+                }
+            }
+            .navigationTitle("Favorites")
+        }
+        
+    }
+}
--- PeraWalletAssignment/Repository/View/RepositoryView.swift
+++ PeraWalletAssignment/Repository/View/RepositoryView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct RepositoryView: View {
     @StateObject var viewModel = RepositoryViewModel()
+    @State private var showingFavorites = false
     
     var body: some View {
         NavigationView {
@@ -18,8 +19,8 @@ struct RepositoryView: View {
                     Picker("Filter by", selection: $viewModel.filterOption) {
                         Text("Name").tag(FilterOption.name)
                         Text("Organization").tag(FilterOption.organization)
-                                        }
-                                        .pickerStyle(DefaultPickerStyle())
+                    }
+                    .pickerStyle(DefaultPickerStyle())
                 }
                 List(viewModel.isSearching ? viewModel.filteredRepositories : viewModel.repositories) { repo in
                     NavigationLink(destination: RepositoryDetailView(repository: repo)) {
@@ -37,25 +38,35 @@ struct RepositoryView: View {
                                 } else {
                                     viewModel.addFavorite(repository: repo)
                                 }
-                            })
-                         {
+                            }) {
                                 Image(systemName: viewModel.favoriteRepositories.contains(where: { $0.id == repo.id }) ? "star.fill" : "star")
                                     .foregroundColor(viewModel.favoriteRepositories.contains(where: { $0.id == repo.id }) ? .yellow : .gray)
-                         }.buttonStyle(BorderlessButtonStyle())
+                            }
+                            .buttonStyle(BorderlessButtonStyle())
                         }
                     }
                 }
-
                 .navigationTitle("Repositories")
+                .navigationBarItems(trailing:
+                    Button(action: {
+                        showingFavorites.toggle()
+                    }) {
+                        Image(systemName: "star.fill")
+                            .foregroundColor(showingFavorites ? .yellow : .gray)
+                    }
+                )
             }
             .onChange(of: viewModel.searchText) { newValue in
-                                viewModel.filterRepositories(searchText: newValue)
-                            }
+                viewModel.filterRepositories(searchText: newValue)
+            }
             .onChange(of: viewModel.filterOption) {newValue in
                 viewModel.searchText = ""
             }
             .onAppear {
                 viewModel.fetchRepositories()
+            }
+            .sheet(isPresented: $showingFavorites) {
+                FavoritesView(viewModel: viewModel)
             }
         }
     }
